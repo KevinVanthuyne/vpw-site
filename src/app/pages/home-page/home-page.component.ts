69a9408e1400d6a6183a47171e2d2b76ccbb0c06
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-05-20 16:11:40 +0200

    Started implementing a Vivus animation

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import * as Vivus from "vivus";
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    new Vivus('globe-svg', {
+      duration: 100,
+      animTimingFunction: Vivus.EASE,
+      file: '/assets/img/globe_5.svg',
+      onReady: (myVivus) => {
+        console.log("LETS GO")
+        // `el` property is the SVG element
+        myVivus.el.setAttribute('height', '100%');
 
-  constructor() { }
-
-  ngOnInit(): void {
+      }
+    });
   }
-
 }
